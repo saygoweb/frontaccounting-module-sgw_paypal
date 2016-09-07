c192a7d1305f147d7f45092f750cbfc684eabcad
Cambell <cambell.prince@gmail.com>
2016-09-07 23:10:19 +0700

    build: gulpfile propage error for test-php

--- gulpfile.js
+++ gulpfile.js
@@ -233,7 +233,7 @@ gulp.task('test-restore', function() {
 gulp.task('test-php', ['env-test'], function(cb) {
   var command = '/usr/bin/env php _frontaccounting/modules/tests/vendor/bin/phpunit -c tests/phpunit.xml';
   execute(command, null, function(err) {
-    cb(null); // Swallow the error propagation so that gulp doesn't display a nodejs backtrace.
+    cb(err); // Propagate the error propagation so that gulp fails for Travis.
   });
 });
 
