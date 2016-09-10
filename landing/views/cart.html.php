4e91d53a6abd6393ee25943952e75e686f990795
Cambell <cambell.prince@gmail.com>
2016-09-10 22:46:19 +0700

    wip: landing theme (basic bootstrap)

--- landing/views/cart.html.php
+++ landing/views/cart.html.php
@@ -2,14 +2,22 @@
 // use SGW_Landing\DebtorTransModel;
 // $cart = new DebtorTransModel()
 ?>
+<div class="page-header">
+	<h1>Invoice <span class="text-muted small"><?= $invoice->reference ?></span></h1>
+</div>
+
+<form class="form">
+<form-group>
+</form-group>
+</form>
 Reference: <?= $invoice->reference ?>
 <?= $invoice->dueDate ?>
-<table>
-<th>
-<td></td>
-<td></td>
-<td></td>
-</th>
+<table class="table">
+<tr>
+<th>Description</th>
+<th>Price</th>
+<th>Quantity</th>
+</tr>
 <?php foreach($invoice->_items as $item): ?>
 <tr>
 <td><?= $item->description ?></td>
@@ -18,4 +26,6 @@ Reference: <?= $invoice->reference ?>
 </tr>
 <?php endforeach; ?>
 </table>
+<div class="pull-right">
 <?= $paynow ?>
+</div>
--- landing/views/page.html.php
+++ landing/views/page.html.php
@@ -1,9 +1,71 @@
-<html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="../../favicon.ico">
+<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap-theme.min.css" />
+<link rel="stylesheet" type="text/css" href="/assets/css/site.css" />
+
+<title>Invoice Payment</title>
+
+</head>
 <body>
-<div>Header etc</div>
-<div>
-<?= $content; ?>
-</div>
-<div>Footer etc</div>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed"
+					data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+					aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span> <span
+						class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><img src="/assets/images/sgw-logo.png" style="height: 28px;margin-top:-6px"/></a>
+				<a class="navbar-brand" href="#">Project name</a>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#contact">Contact</a></li>
+					<li class="dropdown"><a href="#" class="dropdown-toggle"
+						data-toggle="dropdown" role="button" aria-haspopup="true"
+						aria-expanded="false">Dropdown <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Action</a></li>
+							<li><a href="#">Another action</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li role="separator" class="divider"></li>
+							<li class="dropdown-header">Nav header</li>
+							<li><a href="#">Separated link</a></li>
+							<li><a href="#">One more separated link</a></li>
+						</ul></li>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</nav>
+	<div class="container">
+		<div>
+		<?= $content; ?>
+		</div>
+	</div>
+
+	<footer class="footer">
+		<div class="container">
+			<p class="text-muted">Place sticky footer content here.</p>
+		</div>
+	</footer>
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="/assets/js/jquery-1.10.2.min.js"></script><script src="/assets/js/bootstrap.min.js"></script>
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="/assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
